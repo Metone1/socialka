acac25eaf2a3305bfa6e9f5fb9ca9bf54a85f958
Metone1 <Evgeny.Chernov.One@gmail.com>
2022-04-26 21:38:29 +0300

    Navigation refactoring

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,28 @@
 import styles from './Navigation.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
     return (
         <nav className={styles.nav}>
-            <div><img src="https://cdn-icons-gif.flaticon.com/6172/6172532.gif" /><a href='/profile'>Profile</a></div>
-            <div><img src="https://cdn-icons-gif.flaticon.com/6416/6416396.gif" /><a href='/dialogs'>Messages</a></div>
-            <div><img src="https://cdn-icons-gif.flaticon.com/6454/6454239.gif" /><a href='/news'>News</a></div>
-            <div><img src="https://cdn-icons-gif.flaticon.com/6172/6172517.gif" /><a href='/music'>Music</a></div>
+            <div>
+                <img src='https://cdn-icons-gif.flaticon.com/6172/6172532.gif' /><NavLink className={(navData) => navData.isActive ? styles.active : ''} to='/profile'>Profile</NavLink>
+            </div>
+            <div>
+                <img src='https://cdn-icons-gif.flaticon.com/6416/6416396.gif' /><NavLink className={(navData) => navData.isActive ? styles.active : ''} to='/dialogs'>Messages</NavLink>
+            </div>
+            <div>
+                <img src='https://cdn-icons-gif.flaticon.com/6454/6454239.gif' /><NavLink className={(navData) => navData.isActive ? styles.active : ''} to='/news'>News</NavLink>
+            </div>
+            <div>
+                <img src='https://cdn-icons-gif.flaticon.com/6172/6172517.gif' /><NavLink className={(navData) => navData.isActive ? styles.active : ''} to='/music'>Music</NavLink></div>
             <br />
-            <div><img src="https://cdn-icons-gif.flaticon.com/6569/6569144.gif" /><a href='/settings'>Settings</a></div>
+            <div>
+                <img src='https://cdn-icons-gif.flaticon.com/6569/6569144.gif' /><NavLink className={(navData) => navData.isActive ? styles.active : ''} to='/settings'>Settings</NavLink>
+            </div>
         </nav>
     );
 }
+
+console.log(styles)
 
 export default Navigation;
