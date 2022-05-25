ee60d3ee49e17f6d47a2987e8771f3b48ff99e3f
Metone1 <Evgeny.Chernov.One@gmail.com>
2022-05-26 02:06:38 +0300

    switch case default++

--- src/store/messages_reducer.js
+++ src/store/messages_reducer.js
@@ -3,17 +3,23 @@ const sendMessage = 'SEND_MESSAGE';
 let counter = 3;
 
 const messagesReducer = (state, action) => {
-    if (action.type === updateNewMessageBody) {
-        state.newMessageBody = action.body;
+    switch (action.type) {
+        case updateNewMessageBody:
+            state.newMessageBody = action.body;
+            return state;
+        case sendMessage:
+            if (state.newMessageBody === '') return state;
+            else {
+                let body = state.newMessageBody;
+                state.newMessageBody = '';
+                state.usersData.push({ id: ++counter, img: 'https://i.pinimg.com/736x/1f/f5/72/1ff572cda8eaaa77a55c519c4cf80779.jpg', name: 'm', message: body });
+                ;
+                return state;
+            }
+        default:
+            return state;
     }
-    else if (action.type === sendMessage) {
-        let body = state.newMessageBody;
-        state.newMessageBody = '';
-        state.usersData.push({ id: ++counter, img: 'https://i.pinimg.com/736x/1f/f5/72/1ff572cda8eaaa77a55c519c4cf80779.jpg', name: 'm', message: body });
-    };
-    return state;
 };
-
 export const sendMessageCreator = (id) => ({ type: sendMessage });
 export const updateNewMessageBodyCreator = (bodyMessage) => ({ type: updateNewMessageBody, body: bodyMessage });
 
